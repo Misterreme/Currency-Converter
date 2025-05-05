be855d800156933e5c44d90ef093eb511804d550
Misterreme <nunezemmanuel107@gmail.com>
2025-05-05 17:43:10 -0400

    Commit inicial

---
+++ README.md
@@ -0,0 +1,34 @@
+# 💱 Currency Converter
+
+A simple web application that allows users to convert between different currencies using up-to-date exchange rate data from an external API.
+
+## 🚀 Features
+
+- Real-time currency conversion (e.g., USD → EUR)
+- Input validation to prevent empty or invalid fields
+- Clean and user-friendly interface
+- Integration with a public API to fetch live exchange rates
+
+## 🛠️ Technologies Used
+
+- HTML, CSS, and JavaScript
+- [Currency Conversion and Exchange Rates API](https://rapidapi.com/principalapis/api/currency-conversion-and-exchange-rates) via RapidAPI
+
+## 📷 Screenshot
+
+> [Screnshot]()
+
+## ⚙️ How to Use
+
+### Enter:
+
+- The currency to convert from (e.g., USD)
+- The currency to convert to (e.g., EUR)
+- The amount to convert
+- Click "Convert" to get the conversion result.
+
+### Example: 
+```From: USD
+To: EUR
+Amount: 100
+Result: 92.35 EUR
---
+++ src/scripts/index.js
@@ -0,0 +1,113 @@
+const fromSelect = document.getElementById("from-currency");
+const toSelect = document.getElementById("to-currency");
+const amountInput = document.getElementById("amount-input");
+const convertButton = document.getElementById("convert-button");
+const convertForm = document.getElementById("convert-form");
+const totalInput = document.getElementById("total-input");
+const totalText = document.getElementById("total-text");
+const changeCurrencyButton = document.getElementById("change-currency");
+const toggleThemeButton = document.getElementById("theme-button");
+const html = document.querySelector("html")
+
+const API = 'https://currency-conversion-and-exchange-rates.p.rapidapi.com/convert?';
+const options = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': 'd23e0d6053msha5781ab07a8be5ap1752f8jsn84b8caaeaf87',
+		'x-rapidapi-host': 'currency-conversion-and-exchange-rates.p.rapidapi.com'
+	}
+};
+
+changeCurrencyButton.addEventListener("click", (changeCurrency));
+convertButton.addEventListener("click", app);
+toggleThemeButton.addEventListener("click", toggleTheme);
+
+const currentTheme = localStorage.getItem("theme");
+function toggleTheme() {
+    const isDark = html.classList.toggle("dark");
+
+    if (isDark) {
+        html.classList.remove("light");
+        html.style.colorScheme = "dark";
+    } else {
+        html.classList.add("light");
+        html.style.colorScheme = "light";
+    }
+
+    const theme = html.classList.contains("dark") ? "dark" : "light";
+    localStorage.setItem("theme", theme);
+}
+
+if (currentTheme === "dark") {
+    html.classList.add("dark");
+    html.style.colorScheme = "dark"
+} 
+
+function changeCurrency() {
+    let fromCurrency = fromSelect.value;
+    let toCurrency = toSelect.value;
+
+    fromSelect.value = toCurrency
+    toSelect.value = fromCurrency
+};
+
+async function fetchData(urlApi) {
+    const response = await fetch(urlApi, options);
+    const data = await response.json();
+    return data;
+};
+
+async function app(event) {
+    event.preventDefault();
+
+    const data = await consumeApi();
+
+    if (data === undefined) {
+        showError("Complete this field!");
+        return;
+    }
+    
+    totalText.style.display = "block";
+    totalInput.style.display = "block";
+    totalInput.value = data.result;
+    hiddeError();
+
+    return data;
+};
+
+async function consumeApi() {
+    const from = fromSelect.value;
+    const to = toSelect.value;
+    const amount = amountInput.value;
+
+    if (!from || !to || !amount) {
+        showError("Complete this field!");
+        return
+    }
+
+    try {
+
+        const data = await fetchData(`${API}from=${from}&to=${to}&amount=${amount}`);
+        console.log(data.result);
+        hiddeError();
+        return data;
+
+    } catch (error) {
+        console.error(error);
+
+    };
+};
+
+function showError(message) {
+    amountInput.classList.add("error")
+    amountInput.placeholder = message;
+    return;
+};
+
+function hiddeError() {
+    amountInput.classList.remove("error")
+    amountInput.placeholder = "";
+    return; 
+};
+
+
